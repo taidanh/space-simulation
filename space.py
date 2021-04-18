f64fa48dcf075f98200de57aca29e395b1c39649
Tai Danh <tmdanh@ucsc.edu>
2021-04-17 20:02:16 -0700

    add planets and can pause

--- space.py
+++ space.py
@@ -2,6 +2,7 @@
 from Planet import *
 import Universe
 
+from random import randint
 import numpy as np
 import pygame
 from pygame.locals import *
@@ -9,11 +10,14 @@ from pygame.locals import *
 #           name           pos        vel       radius   color       surface_gravity
 p1 = Planet("walnut",   (640, 360), (2, 0),       50, (0, 0, 255), 7000)
 p2 = Planet("bruh",     (640, 450), (-400, 0),    15, (0, 255, 0), 10)
-p3 = Planet("tucan",    (800, 200), (-200, -200), 30, (255, 0, 0), 200)
+p3 = Planet("raq",    (800, 200), (-200, -200), 30, (255, 0, 0), 200)
 p4 = Planet("moon",     (800, 155), (-290, -160), 5,  (115, 115, 115), 50)
 
 all_planets: [Planet] = [p1, p2, p3, p4]
 radius: int = 20
+
+def rand_color():
+    return (randint(0, 255), randint(0, 255), randint(0, 255))
 
 #--------------#
 # pygame stuff #
@@ -32,25 +36,10 @@ background = pygame.Surface(window)
 running = True
 pause = True
 
-# press SPACE to start simulation
-while pause:
-    for event in pygame.event.get():
-        if event.type == KEYDOWN:
-            if event.key == K_SPACE:
-                pause = False
 
 while running:
 
     dt = clock.tick(60)
-
-    # check if user closed game
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            print("quitting game...")
-            running = False
-        elif event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                running = False
 
     # black background
     screen.fill((0, 0, 0))
@@ -62,10 +51,9 @@ while running:
 
     # do physics on all planets
     for planet in all_planets:
-        planet.update_velocity(all_planets, Universe.TIME_STEP)
-        # print("{}'s vel =\t[{:.3f}, {:.3f}],\tnormed = {}".format(
-        #    planet.name, planet.v[0], planet.v[1], planet.get_vel()))
-        planet.update_position(Universe.TIME_STEP)
+        if (not pause):
+            planet.update_velocity(all_planets, Universe.TIME_STEP)
+            planet.update_position(Universe.TIME_STEP)
         pygame.draw.circle(screen, planet.get_color(), planet.get_pos(), planet.get_radius())
         pygame.draw.line(screen, (115, 115, 115), planet.get_pos(), vec_add(planet.get_pos(), vec_mul(planet.get_vel(), 30)))
         text = font_renderer.render(planet.name, True, (255, 255, 255))
@@ -77,5 +65,26 @@ while running:
 
     # flip the display
     pygame.display.flip()
+
+
+    # check if user closed game
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            print("quitting game...")
+            running = False
+        elif event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+            if event.key == K_SPACE:
+                pause ^= 1
+        elif event.type == MOUSEBUTTONDOWN:
+            if event.button == 4:
+                radius += 2
+            if event.button == 5:
+                if (radius - 2) > 0:
+                    radius -= 2
+            if event.button == 1:
+                all_planets.append(Planet("unnamed", pygame.mouse.get_pos(), (randint(-100, 100), randint(-100, 100)), radius,  rand_color(), 15))
+
 
 pygame.quit()
